9fa6ec555646e5e930a1f6ab6c14255127a8e68c
DuqueMajores <duque.majores@gmail.com>
2025-04-11 12:53:19 -0300

    11/04/2025

--- aula08 - Tipo Boolean/aula08_tipo_boolean.py
+++ aula08 - Tipo Boolean/aula08_tipo_boolean.py
@@ -1,1 +1,26 @@
-#Ola MUndo
+aula = True
+print(aula)         #True
+
+aula = 10 > 15
+print(aula)         #False
+
+aula = ""
+print(bool(aula))   #False
+if aula:
+    print("Possui texto")
+else:
+    print("Vazio")
+
+
+aula = "CFB Curso"
+if aula:
+    print("Possui texto")
+else:
+    print("Vazio")
+
+
+aula = 0
+print(bool(aula))  #False
+
+aula = 1
+print(bool(aula))  #True
